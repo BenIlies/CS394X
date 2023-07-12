f98203be20b3ade7476734231e2ce420f001583e
Ilies Benhabbour <iliesbenh@gmail.com>
2023-07-12 10:12:54 +0300

    Clean conf.py and add report name and authors

--- conf.py
+++ conf.py
@@ -1,28 +1,6 @@
-import os
-import shutil
-
-# Define the root and destination paths
-root_path = os.path.dirname(os.path.abspath(__file__))
-docs_path = os.path.join(root_path, 'docs')
-
-# Create the "docs" folder if it doesn't exist
-if not os.path.exists(docs_path):
-    os.makedirs(docs_path)
-
-# Recursively copy Markdown files from the root folder (excluding "docs") to "docs" folder
-for root, dirs, files in os.walk(root_path):
-    if 'docs' in dirs:
-        dirs.remove('docs')  # Exclude the "docs" folder from further traversal
-    for file in files:
-        if file.endswith('.md'):
-            src_path = os.path.join(root, file)
-            dst_dir = os.path.join(docs_path, os.path.relpath(root, root_path))
-            dst_path = os.path.join(dst_dir, file)
-
-            if not os.path.exists(dst_dir):
-                os.makedirs(dst_dir)
-
-            shutil.copyfile(src_path, dst_path)
+project = 'CS394X Course Report: Attacks and Mitigations'
+copyright = '2023, Ilies Benhabbour & Krishnan Iyer'
+author = 'Ilies Benhabbour & Krishnan Iyer'
 
 extensions = ['myst_parser']
 
